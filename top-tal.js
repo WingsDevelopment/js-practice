0d76c6d5ac4e8710ec91fe312bcd89f4db5c60d5
WingsDevelopment <bans.cowboy@gmail.com>
2021-10-09 00:12:48 +0200

    c

--- top-tal.js
+++ top-tal.js
@@ -14,8 +14,38 @@ function foo2()
   };
 }
 
-console.log("foo1 returns:");
-console.log(foo1()); // { bar: hello }
-console.log("foo2 returns:");
-console.log(foo2()); //undefined
+// console.log("foo1 returns:");
+// console.log(foo1()); // { bar: hello }
+// console.log("foo2 returns:");
+// console.log(foo2()); //undefined
 //enter radi kao kraj statementa
+
+var length = 10;
+var g = this;
+function fn() {
+	console.log(5);
+}
+
+var obj = {
+  length: 5,
+  method: function(x) {
+    var a = x.bind(g);
+    a();
+    arguments[0]();
+  }
+};
+
+//obj.method(fn, 1);
+
+
+fn ();
+
+var x = 21;
+var girl = function () {
+    console.log(x);
+    var x = 20;
+};
+girl ();
+
+b++
+var b = 5
